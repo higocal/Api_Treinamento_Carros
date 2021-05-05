91f710c5cf1350df14293b2277773bb34ea4a413
Higor <calesso15@hotmail.com>
2021-05-05 08:48:52 -0300

    Versão DEV

--- src/main/java/com/carros/api/IndexController.java
+++ src/main/java/com/carros/api/IndexController.java
@@ -13,7 +13,7 @@ public class IndexController {
 
     @GetMapping()
     public String get() {
-        return "API dos Carros Versão 1";
+        return "API dos Carros Versão DEV";
     }
 
     @GetMapping("/userInfo")
